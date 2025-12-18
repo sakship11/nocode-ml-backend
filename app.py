ad8b57030ac4f1ec700ed1965ad45dc3db2e89a6
Sakshi <sakshipawar112004@gmail.com>
2025-12-18 12:01:08 +0530

    Fix backend root route

--- app.py
+++ app.py
@@ -164,7 +164,5 @@ def reset():
     X_train = X_test = y_train = y_test = None
     return jsonify({"message": "Analysis reset successfully"})
 
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
